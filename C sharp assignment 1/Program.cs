b70dd8247d4d41518dbb65a476dddc50c6e984d1
Stephanie Schulz <ohclemintine21@gmail.com>
2020-05-04 14:39:08 -0700

    all files

--- C sharp assignment 1/Program.cs
+++ C sharp assignment 1/Program.cs
@@ -1,14 +1,9 @@
-﻿using System;
-
-
+using System;
 
 class Program
 {
-    static void Main()
-    {
-        Console.WriteLine("Welcome to Acme Accounting Systems");
-        Console.ReadLine();
-        Console.WriteLine("Remember, we're 'accounting' on you");
-        Console.ReadLine();
-    }
+	static void Main()
+	{
+		Console.WriteLine("Hello, World");
+	}
 }
