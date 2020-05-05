ce45318a3a6a01ffcbcd06decb158cc578568ecf
Ohclemintine21 <60559963+Ohclemintine21@users.noreply.github.com>
2020-05-04 21:06:03 -0700

    added new files c# basics assignments

---
+++ WelcomeToAcmeSystems/HelloWorld2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+        Console.ReadLine();
+        Console.WriteLine("Remember, we're 'accounting' on you");
+        Console.ReadLine();
+        }
+    }
+
