d321098291778cbf91389d64fa37187aece837bf
Stephanie Schulz <ohclemintine21@gmail.com>
2020-06-05 18:47:15 -0700

    merge

--- ReportForStudents/ReportForStudents/Program.cs
+++ ReportForStudents/ReportForStudents/Program.cs
@@ -15,10 +15,13 @@ namespace ReportForStudents
             Console.WriteLine("The Tech Academy");
             Console.WriteLine("Student Daily Report");
             Console.ReadLine();
+
             Console.WriteLine("What Course are you on?");
-            Console.ReadLine();
+            string usercourse = Console.ReadLine();
+
+            
             Console.WriteLine("What Page are you on?");
-            var pageNum = Console.ReadLine();
+            int pageNum = Convert.ToInt32(Console.ReadLine());
 
             int minPage = 1;
             int maxPage = 500;
@@ -38,16 +41,16 @@ namespace ReportForStudents
                 {
                     Console.WriteLine("");
                 }
-                Console.ReadLine();
+                string userHelp = Console.ReadLine();
 
                 Console.WriteLine("Were there any positive experiences you'd like to share? Please give specifics.");
-                Console.ReadLine();
+                string userExperience = Console.ReadLine();
 
                 Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
-                Console.ReadLine();
+                string userFeedback = Console.ReadLine();
 
                 Console.WriteLine("How many hours did you study?");
-                Console.ReadLine();
+                int userPage = Convert.ToInt32(Console.ReadLine());
 
                 Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
                 Console.ReadLine();
