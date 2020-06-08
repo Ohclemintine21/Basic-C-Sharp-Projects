2da7f0cb0902230164f463a37019b4b39e89094b
Stephanie Schulz <ohclemintine21@gmail.com>
2020-06-07 17:30:34 -0700

    Boolean Operator Assignment
    Boolean operator Assignment1

---
+++ Bool Assignment 1/Bool Assignment 1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bool_Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput1;
+            int intVal;
+            
+            Console.WriteLine("What is your age?");
+            userInput1 = Console.ReadLine();
+            //converts to integer type
+            intVal = Convert.ToInt32(userInput1);
+            Console.WriteLine("{0}", intVal);
+            Console.ReadLine();
+
+            // Determine DUI
+            Console.WriteLine("Have you ever had a DUI?");
+
+            bool yes = true;
+            bool no = false;
+
+            if (no == false)
+            {
+                Console.WriteLine("false");
+            }
+            else if (yes == true)
+            {
+                Console.WriteLine("true");
+            }
+            Console.ReadLine();
+
+            string userInput2;
+            Console.WriteLine("How many speeding tickets do you have?");
+            userInput2 = Console.ReadLine();
+            Console.WriteLine("{0}", userInput2);
+            Console.ReadLine();
+
+            string Qualify;
+            Console.WriteLine("Qualified?");
+            Qualify = Console.ReadLine();
+
+            if (Qualify.Equals(yes))
+            {
+                Console.WriteLine("True");
+            }
+            else if (Qualify.Equals(no))
+            {
+                Console.WriteLine("false");
+            }
+            Console.ReadLine();
+
+           
+           
+
+        }
+    }
+}
---
+++ Bool Examples/Bool Examples/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bool_Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(true && false); //console = false (and operator &&)
+            Console.WriteLine(true && true); //console = true
+            Console.WriteLine(false && false); //console = false
+
+            Console.WriteLine(true && true && true && false); //console = false (all have to be true)
+            Console.WriteLine(true && true && true || false); // console = true (with the or || added)
+
+            Console.WriteLine(true || true); //console = true (or operator ||)
+            Console.WriteLine(true || false); //console = true (bc one is true = true)
+            Console.WriteLine(false || false); //console = false
+
+            Console.WriteLine(true == true); // console = true (equals operator ==)
+            Console.WriteLine(true == false); //console = false
+            Console.WriteLine(false == false); //console = false
+
+            Console.WriteLine(true != true); //console = false (does not equal operator !=)
+            Console.WriteLine(true != false); // console = true 
+            Console.WriteLine(false != false); //console = false
+
+            Console.WriteLine(true ^ true); // console = false  (x or operator) 
+            Console.WriteLine(true ^ false); // console = true (will be true if one is true but not both)
+            Console.WriteLine(false ^ false); // console = false
+
+            Console.ReadLine();
+        }
+    }
+}
