61c944549f761845d0f12fd434a5c06c9ecc7e4f
Stephanie Schulz <ohclemintine21@gmail.com>
2020-06-09 10:05:04 -0700

    Fix variables

--- MathAndComparisonAssignment2/MathAndComparisonAssignment2/Program.cs
+++ MathAndComparisonAssignment2/MathAndComparisonAssignment2/Program.cs
@@ -19,11 +19,13 @@ namespace MathAndComparisonAssignment2
             Console.ReadLine();
 
             Console.WriteLine("Hourly Rate?");
-            Console.WriteLine(15);
+            int hourlyRate = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(hourlyRate);
             Console.ReadLine();
 
             Console.WriteLine("Hours worked per week?");
-            Console.WriteLine(40);
+            int hoursWorked = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(hoursWorked);
             Console.ReadLine();
 
             //Person 2 Details
@@ -31,37 +33,35 @@ namespace MathAndComparisonAssignment2
             Console.ReadLine();
 
             Console.WriteLine("Hourly Rate?");
-            Console.WriteLine(20);
+            int hourlyRate2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(hourlyRate2);
             Console.ReadLine();
 
             Console.WriteLine("Hours worked per week?");
-            Console.WriteLine(40);
+            int hoursWorked2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(hoursWorked2);
             Console.ReadLine();
 
             //Annual Salary Person 1
             Console.WriteLine("Annual Salary of Person 1:");
-            Console.WriteLine(31200);
+            int AnnSalary1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(AnnSalary1);
             Console.ReadLine();
 
             //Annual Salary Person 2
             Console.WriteLine("Annual Salary of Person 2:");
-            Console.WriteLine(41600);
+            int AnnSalary2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(AnnSalary2);
             Console.ReadLine();
 
             //Compare Salaries Person 1 to Person 2
             Console.WriteLine("Does Person 1 make more money than Person 2?");
             Console.ReadLine();
 
-            bool no = false;
-            bool yes = true;
-            if (no == false)
-            {
-                Console.WriteLine("false");
-            }
-            else if (yes == true)
-            {
-                Console.WriteLine("true");
-            }
+            int Person1 = 31200;
+            int Person2 = 41600;
+            bool compareResult = (Person1 > Person2);
+            Console.WriteLine(compareResult);
             Console.ReadLine();
         }
     }
