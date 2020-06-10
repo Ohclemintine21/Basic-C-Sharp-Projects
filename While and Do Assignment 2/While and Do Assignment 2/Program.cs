7c4489a212376ba9483c099d7ad7a77f68ef3b7d
Stephanie Schulz <ohclemintine21@gmail.com>
2020-06-10 00:23:34 -0700

    While and do assignment 2

---
+++ While and Do Assignment 2/While and Do Assignment 2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_and_Do_Assignment_2
+{
+    class Program
+    {
+        static void Main(string[] args, string whale)
+        {
+            Console.WriteLine("Guess the animal?");
+            string animal = Console.ReadLine();
+            bool GuessedRight = animal == whale;
+
+            do
+            {
+                switch (animal)
+                {
+                    case bird:
+                        Console.WriteLine("You guessed bird. Try again.");
+                        Console.WriteLine("Guess the animal?");
+                        animal = Console.ReadLine();
+                        break;
+                    case cat:
+                        Console.WriteLine("You guessed cat. Try again.");
+                        Console.WriteLine("Guess the animal?");
+                        animal = Console.ReadLine();
+                        break;
+                    case dog:
+                        Console.WriteLine("You guessed dog. Try again.");
+                        Console.WriteLine("Guess the animal?");
+                        animal = Console.ReadLine();
+                        break;
+                    case whale:
+                        Console.WriteLine("You guessed Whale. That is correct!");
+                        GuessedRight = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are incorrect.");
+                        Console.WriteLine("Guess the animal?");
+                        animal = Console.ReadLine();
+                        break;
+                }
+            }
+            while (!GuessedRight);
+            Console.ReadLine();
+        }
+    }
+}
