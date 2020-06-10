6f3c979d48caae1c23277540b70938b948f40f64
Stephanie Schulz <ohclemintine21@gmail.com>
2020-06-09 23:11:49 -0700

    While loop

---
+++ While and do loop Assignment/While and do loop Assignment/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_and_do_loop_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess the food?");
+            string food = Console.ReadLine();
+            bool GuessedCorrect = food == pizza;
+
+            while (!GuessedCorrect)
+            {
+                switch (food)
+                {
+                   case taco:
+                      Console.WriteLine("You guessed taco. Try again.");
+                      Console.WriteLine("Guess the food?");
+                      food = Console.ReadLine();
+                      break;
+                    case sushi:
+                        Console.WriteLine("You guessed sushi. Try again.");
+                        Console.WriteLine("Guess the food?");
+                        food = Console.ReadLine();
+                        break;
+                    case burrito:
+                        Console.WriteLine("You guessed burrito. Try again.");
+                        Console.WriteLine("Guess the food?");
+                        food = Console.ReadLine();
+                        break;
+                    case pizza:
+                        Console.WriteLine("You guessed pizza. That is correct!");
+                        GuessedCorrect = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are incorrect.");
+                        Console.WriteLine("Guess the food?");
+                        food = Console.ReadLine();
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
