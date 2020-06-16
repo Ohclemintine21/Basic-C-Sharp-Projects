355d80935d535b2709e9fe1c63f245fb90b64206
Stephanie Schulz <ohclemintine21@gmail.com>
2020-06-15 20:21:36 -0700

    fixed user input errors

--- ArraysAndListsAssignment1/ArraysAndListsAssignment1/Program.cs
+++ ArraysAndListsAssignment1/ArraysAndListsAssignment1/Program.cs
@@ -12,16 +12,20 @@ namespace ArraysAndListsAssignment1
         {
             //User chooses an index from the array and then it displays the string at that index on the screen
             Console.WriteLine("Choose a food!");
+            var f = Convert.ToInt32(Console.ReadLine());
+
             List<string> intList = new List<string>();
             intList.Add("Pizza");
             intList.Add("Cake");
             intList.Add("Burrito");
 
-            Console.WriteLine(intList[0]);
+            Console.WriteLine(intList[f]);
             Console.ReadLine();
 
             //User chooses an index from the array and then it displays the integer at that index on the screen
             Console.WriteLine("Choose a number!");
+            var num = Convert.ToInt32(Console.ReadLine());
+
             int[] numArray = new int[5];
             numArray[0] = 6;
             numArray[1] = 8;
@@ -29,7 +33,8 @@ namespace ArraysAndListsAssignment1
             numArray[3] = 40;
             numArray[4] = 50;
 
-            Console.WriteLine(numArray[0]);
+
+            Console.WriteLine(numArray[num]);
 
             //Error message display if number does not exist
 
@@ -47,12 +52,16 @@ namespace ArraysAndListsAssignment1
 
             //Ask user to select an index of the list and then display the content at that index on the screen
             Console.WriteLine("Choose a Character.");
+            var c = Convert.ToInt32(Console.ReadLine());
+
             List<string> intList2 = new List<string>();
             intList2.Add("Peter Pan");
             intList2.Add("Cinderella");
             intList2.Add("Bugs Bunny");
 
-            Console.WriteLine(intList2[0]);
+       
+
+            Console.WriteLine(intList2[c]);
             Console.ReadLine();
 
         }
