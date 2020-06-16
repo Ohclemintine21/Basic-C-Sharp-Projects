656e32e999348347a9eef23806bd4f521b798939
Stephanie Schulz <ohclemintine21@gmail.com>
2020-06-15 17:30:57 -0700

    fixed bool errors

--- Bool Assignment 1/Bool Assignment 1/Program.cs
+++ Bool Assignment 1/Bool Assignment 1/Program.cs
@@ -10,55 +10,51 @@ namespace Bool_Assignment_1
     {
         static void Main(string[] args)
         {
+            //user input age 
             Console.WriteLine("What is your age?");
             int Age = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(Age);
             Console.ReadLine();
 
-
+            //user input number of DUI's
             Console.WriteLine("How you ever had a DUI?");
-            string userDUI = Console.ReadLine();
+            var userDUI = Console.ReadLine();
 
-            if (userDUI == "Yes") 
+            if (userDUI == "Yes")
             {
-                bool answer = true;
-                Console.WriteLine(answer);
+                Console.WriteLine("True");
             }
-            else if (userDUI == "No")
+            else if (userDUI != "No")
             {
-                bool answer2 = false;
-                Console.WriteLine(answer2);
+                Console.WriteLine("False");
             }
+            
             Console.ReadLine(); 
 
+
+            // user input number of speeding tickets
             Console.WriteLine("How many speeding tickets do you have?");
             int numTickets = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine(numTickets);
             Console.ReadLine();
 
+             
+            // find out if the user is qualified for insurance
             Console.WriteLine("Qualified?");
 
             bool isQualified = true;
-            bool isNotQualified = false;
-
-            if (Age >= 15)
-            {
-                Console.WriteLine(isQualified);
-            }
-            else if (numTickets <= 1)
-            {
-                Console.WriteLine(isQualified);
-            }
-            else if (userDUI == "No") 
+            bool notQualified = false;
+           
+            if (Age >= 15 && numTickets <= 1 && userDUI != "No")
             {
                 Console.WriteLine(isQualified);
             }
             else
             {
-                Console.WriteLine(isNotQualified);
+                Console.WriteLine(notQualified);
             }
-
             Console.ReadLine();
+
         }
     }
 }
