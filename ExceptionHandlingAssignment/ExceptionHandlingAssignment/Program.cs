bac25e3ab1e953c381ec48b597246b2e2fc6c185
Stephanie Schulz <ohclemintine21@gmail.com>
2020-07-13 13:03:15 -0700

    fixed integers

--- ExceptionHandlingAssignment/ExceptionHandlingAssignment/Program.cs
+++ ExceptionHandlingAssignment/ExceptionHandlingAssignment/Program.cs
@@ -8,7 +8,7 @@ class Program
     {
         try
         {
-            List<int> Numbers = new List<int>();
+            List<int> Numbers = new List<int>() {2, 6, 12, 24, 36};
             Console.WriteLine("Pick a number.");
             int numberOne = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Pick a second number.");
