6aeed385a94cad5657f1be8f032dfdf82283ced2
Stephanie Schulz <ohclemintine21@gmail.com>
2020-07-27 11:50:45 -0700

    Methods assignment

---
+++ Methods/Methods/Math.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Math
+    {
+        public int FindMath(int num1, int num2)
+        {
+            int result;
+
+            if (num1 > num2)
+                result = num1;
+            else
+                result = num2;
+            return result;
+        }     
+    }
+}
+
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ask user for number and pass to method
+            Console.WriteLine("Please enter the first number to do math on: ");
+            int UserInput1 = Convert.ToInt32(Console.ReadLine());
+
+            //ask for second number to pass 
+            Console.WriteLine("Please enter a second number to do math on: ");
+            int UserInput2 = Convert.ToInt32(Console.ReadLine());
+
+            Math math = new Math();
+
+            int result = math.FindMath(UserInput1, UserInput2);
+            Console.WriteLine("Result of the FindMath method is: " + result);
+            Console.ReadLine();
+        }      
+    }
+}
+       
