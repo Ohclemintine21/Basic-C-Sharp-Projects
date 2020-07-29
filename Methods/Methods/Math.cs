b73443dea27b01c27eff61a18890a23db1ca4117
Stephanie Schulz <ohclemintine21@gmail.com>
2020-07-28 20:00:03 -0700

    Added all required methods

--- Methods/Methods/Math.cs
+++ Methods/Methods/Math.cs
@@ -9,16 +9,22 @@ namespace Methods
 {
     class Math
     {
+        //method 1 add two integers
         public int FindMath(int num1, int num2)
         {
-            int result;
-
-            if (num1 > num2)
-                result = num1;
-            else
-                result = num2;
-            return result;
-        }     
+            return num1 + num2;
+        }
+        //method 2 subtract two integers
+        public static int FindMath2(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        //method 3 multiply two integers
+        public static int FindMath3(int num1, int num2)
+        {
+            return num1 * num2;
+        }
     }
 }
+
 
--- Methods/Methods/Program.cs
+++ Methods/Methods/Program.cs
@@ -12,11 +12,11 @@ namespace Methods
         static void Main(string[] args)
         {
             //ask user for number and pass to method
-            Console.WriteLine("Please enter the first number to do math on: ");
+            Console.WriteLine("Please enter the first number to add: ");
             int UserInput1 = Convert.ToInt32(Console.ReadLine());
 
             //ask for second number to pass 
-            Console.WriteLine("Please enter a second number to do math on: ");
+            Console.WriteLine("Please enter a second number to add: ");
             int UserInput2 = Convert.ToInt32(Console.ReadLine());
 
             Math math = new Math();
@@ -24,7 +24,32 @@ namespace Methods
             int result = math.FindMath(UserInput1, UserInput2);
             Console.WriteLine("Result of the FindMath method is: " + result);
             Console.ReadLine();
-        }      
+
+            //ask user for a number and pass second method
+            Console.WriteLine("Please enter the first number to subtract: ");
+            int userInput1 = Convert.ToInt32(Console.ReadLine());
+
+            //ask user for a second number to pass
+            Console.WriteLine("Please enter a second number to subtract: ");
+            int userInput2 = Convert.ToInt32(Console.ReadLine());
+
+            int result2 = Math.FindMath2(userInput1, userInput2);
+            Console.WriteLine("Result of the subtract method is: " + result2);
+            Console.ReadLine();
+
+            //ask user for a number to pass third method
+            Console.WriteLine("Please enter the first number to multiply: ");
+            int UserInput3 = Convert.ToInt32(Console.ReadLine());
+
+            //ask user for second number to multiply
+            Console.WriteLine("Please enter a second number to multiply: ");
+            int UserInput4 = Convert.ToInt32(Console.ReadLine());
+
+            int result3 = Math.FindMath3(UserInput3, UserInput4);
+            Console.WriteLine("Result of multiply method is: " +result3);
+            Console.ReadLine();
+        }
+        
     }
 }
        
