df48fe11deaab6849ad0863d8ede3bdedfd5618f
Stephanie Schulz <ohclemintine21@gmail.com>
2020-07-30 11:40:44 -0700

    partial completion

---
+++ Method3/Method3/MathMethod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method3
+{
+    class MathMethod
+    {
+        //first method with two integers with last parameter optional
+        public int MathProblem(int num1, int num2, int num3 = 5)
+        {
+            return num1 - num2 - num3;
+        }
+
+    }
+}
---
+++ Method3/Method3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //pass first integer
+            Console.WriteLine("Enter a number to subtract: ");
+            int Input1 = Convert.ToInt32(Console.ReadLine());
+
+            //pass second int
+            Console.WriteLine("Enter a second number to subtract: ");
+            int Input2 = Convert.ToInt32(Console.ReadLine());
+
+            //Instaniate class
+            MathMethod mathMethod = new MathMethod();
+
+            //console results
+            int result = mathMethod.MathProblem(Input1, Input2);
+            Console.WriteLine("Result of MathProblem method is: " + result);
+            Console.ReadLine();
+
+        }
+    }
+}
