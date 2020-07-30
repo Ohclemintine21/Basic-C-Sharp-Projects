0dd12172a1b3d26353c82795a04e2a37cb1793bc
Stephanie Schulz <ohclemintine21@gmail.com>
2020-07-30 15:32:01 -0700

    fix int errors

--- Method3/Method3/Program.cs
+++ Method3/Method3/Program.cs
@@ -12,12 +12,21 @@ namespace Method3
         {
             //pass first integer
             Console.WriteLine("Enter a number to subtract: ");
-            int Input1 = Convert.ToInt32(Console.ReadLine());
+            int Input1 = Convert.ToInt32( Console.ReadLine());
+            
 
             //pass second int
-            Console.WriteLine("Enter a second number to subtract: ");
-            int Input2 = Convert.ToInt32(Console.ReadLine());
-
+            string Input2 = (Console.ReadLine());
+            if (Input2 == " ")
+            {
+                Console.WriteLine(Input1);
+            }
+            else
+            {
+              string input2 = Convert.ToInt32(Console.ReadLine(Input1, Input2));
+            }
+            Console.ReadLine();
+            
             //Instaniate class
             MathMethod mathMethod = new MathMethod();
 
