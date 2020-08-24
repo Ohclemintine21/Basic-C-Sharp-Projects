c9f1412470301f3250f8671f2e1d8f12ac5e9995
Stephanie Schulz <ohclemintine21@gmail.com>
2020-08-23 17:23:10 -0700

    iteration update assignment

--- iterationAssignment/iterationAssignment/Program.cs
+++ iterationAssignment/iterationAssignment/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 class Program
@@ -20,7 +21,7 @@ class Program
         {
             Console.WriteLine(bookNames[i] + UserInput);
         }
-
+        Console.ReadLine();
 
         //Create a loop that prints off each string in the array on separate line
         foreach (string bookName in bookNames)
@@ -40,85 +41,78 @@ class Program
 
         }
         //Create a loop where the comparison used to determine whether to continue iterrating the loop is a "<"
-        int[] ReadHours = { 2, 4, 6, 8, 10 };
-
-
-        for (int i = 0; i < ReadHours.Length; i++)
         {
-            Console.WriteLine(ReadHours[i]);
+            var ReadHours = 5;
+
+            for (int i = 0; i < ReadHours; i++)
+                Console.WriteLine(ReadHours);
         }
         Console.ReadLine();
 
         //Create a loop where the comparison used to determine whether to continue iterating the loop is a "<=" operator.
-        int[] numberBooks = { 4, 6, 8, 10 };
-       
-        
-        for (int i = 0; i <= numberBooks.Length; i++)
         {
-            Console.WriteLine(numberBooks[i]);
+            int numberBooks = 5;
+
+            for (int i = 0; i <= numberBooks; i++)
+                Console.WriteLine(i);
         }
         Console.ReadLine();
 
         //Create a list of strings where each tem is unique
-        List<string> speciesWhales = new List<string>() { "Sperm", "Orca", "Beluga", "Blue", "Humpback" };
-        
+        var speciesWhales = new List<string>() { "Sperm", "Orca", "Beluga", "Blue", "Humpback" };
 
         //Ask user to input text to search for in the list
         Console.WriteLine("Select your favorite whale from the list: ");
-        string userInput = Console.ReadLine();
+        var userInput = Console.ReadLine();
 
         //Create a loop that iterates through the loop and then displays the index of the array that contains matching text on the screen
-        for (int i = 0; i < speciesWhales.Count; i++)
+        foreach (string whale in speciesWhales)
         {
-            if (userInput == speciesWhales[i])
+            if (userInput == whale )
             {
-                Console.WriteLine("Match found at index " + i);
+                Console.WriteLine("Match found at index " +userInput.Length);
+                break;
             }
             //add code that tells user input is not in list
             else
             {
                 Console.WriteLine("Sorry, that input is not in the list of whales. ");
             }
-           
-        }
-        Console.ReadLine();
-        
 
-        //list of strings that has two identical strings in list
-        List<string> pets = new List<string>() { "dog", "dog", "cat", "bird", "fish" };
+            //list of strings that has two identical strings in list
+            List<string> pets = new List<string>() { "dog", "dog", "cat", "bird", "fish" };
 
-        //Ask the user to select text to search for in the list
-        string UserInput2 = Console.ReadLine();
-        Console.WriteLine("Select a pet to search for in the list: " +UserInput2);
+            //Ask the user to select text to search for in the list
+            string UserInput2 = Console.ReadLine();
+            Console.WriteLine("Select a pet to search for in the list: " + UserInput2);
 
-        //for loop that evaluates each item in list and displays all items
-       for (int i = 0; i <pets.Count; i++)
-       {
-            if (UserInput2 == pets[i])
+            //for loop that evaluates each item in list and displays all items
+            for (int i = 0; i < pets.Count; i++)
             {
-                Console.WriteLine(pets.Count);
+                if (UserInput2 == pets[i])
+                {
+                    Console.WriteLine(pets.Count);
+                    break;
+                }
+                //add code to loop that tells user they put in text that isn't in list
+                else
+                {
+                    Console.WriteLine("Sorry that input is not in the list. ");
+                }
             }
-            //add code to loop that tells user they put in text that isn't in list
-            else
+            Console.ReadLine();
+
+            //Create a list of strings with two identical strings in the list
+            List<string> Cars = new List<string>() { "Volvo", "Toyota", "Honda", "Ford", "Honda" };
+
+            //Create a foreach loop that evaluates each item in the list and displays a message showing the string & whether or not it has already appeared in the list
+            foreach (string car in Cars)
             {
-                Console.WriteLine("Sorry that input is not in the list. ");
+                Console.WriteLine("This car has appeared in list already:" + car);
             }
-       }
-       Console.ReadLine();
-
-        //Create a list of strings with two identical strings in the list
-        List<string> Cars = new List<string>() { "Volvo", "Toyota", "Honda", "Ford", "Honda" };
-
-        //Create a foreach loop that evaluates each item in the list and displays a message showing the string & whether or not it has already appeared in the list
-        foreach (string car in Cars)
-        {
-            Console.WriteLine("This car has appeared in list already:" + car);
+            Console.ReadLine();
         }
-        Console.ReadLine();
-        
-
     }
-
 }
     
 
