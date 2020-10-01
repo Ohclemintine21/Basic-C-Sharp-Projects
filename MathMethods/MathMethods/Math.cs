98db5bc333ae514d889753c402cf3d451cba28e7
Stephanie Schulz <ohclemintine21@gmail.com>
2020-09-30 17:48:25 -0700

    Methods assignment 1

---
+++ MathMethods/MathMethods/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMethods
+{
+    class Math
+    {
+    //first method
+     public int MathOp(int val)
+        {
+            return val +2;
+        }
+        //second method
+        public decimal MathOp(decimal val)
+        {
+            return val * 2;
+        }
+        //third method
+        public int MathOp(string val)
+        {
+            return Int32.Parse(val) * 10;
+        }
+
+       
+    }
+}
---
+++ MathMethods/MathMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate first method
+            Math mathOne = new Math();
+            int result = mathOne.MathOp(3);
+            Console.WriteLine(result);
+
+            //instantiate second method
+            decimal decimalValue = 6.2M;
+            decimal result2 = mathOne.MathOp(decimalValue);
+            Console.WriteLine(result2);
+
+            //instantiate third method
+            int result3 = mathOne.MathOp("3");
+            Console.WriteLine(result3);
+            Console.ReadLine();
+
+        }
+    }
+}
