a82df8ae4261845be2778e0989ac782dacfe08ed
Stephanie Schulz <ohclemintine21@gmail.com>
2020-10-15 21:46:09 -0700

    Person Employee assignment

---
+++ ClassFunction/ClassFunction/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassFunction
+{
+    //create a class called Person
+    public class Person
+    {
+        //give it two string properties
+        public string firstName;
+        public string lastName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value;  }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        //give class with void method SayName with no parameters and simply writes persons full name to console
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName);
+        }
+    }
+}
---
+++ ClassFunction/ClassFunction/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassFunction
+{
+    public class Employee : Person
+    {
+        int id;
+
+        public int Id
+        {
+            get { return id;  }
+            set { id = value;  }
+        }
+
+      
+    }
+}
