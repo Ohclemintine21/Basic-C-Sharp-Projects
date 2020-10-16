04faf99d2e3f8606f9076365d823d978cd72c7cb
Stephanie Schulz <ohclemintine21@gmail.com>
2020-10-15 17:12:15 -0700

    Adding two integers

---
+++ MathMethod2/MathMethod2/Math2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMethod2
+{
+    class Math2
+    {
+        //create a method that takes two integers as parameters. Make one optional
+        public int MyMathAdd(int userInt1, int userInt2 = 0)
+        {
+            return userInt1 + userInt2 + 75;
+        } 
+
+    }
+}
---
+++ MathMethod2/MathMethod2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathMethod2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+            //ask user to input two numbers one at a time
+            Console.WriteLine("Please enter a whole number: ");
+            int userInt1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a second whole number (optional): ");
+
+            //instaniate the class
+            Math2 mathInstance = new Math2();
+            int userInt2;
+            int result;
+            if(int.TryParse(Console.ReadLine(), out userInt2))
+            {
+                result = mathInstance.MyMathAdd(userInt1, userInt2);
+            }
+            else
+            {
+                result = mathInstance.MyMathAdd(userInt1);
+            }
+            Console.WriteLine(userInt1 + " plus " + userInt2 + " plus 75 equals " + result + ".");
+            Console.ReadLine();
+        }
+    }
+}
