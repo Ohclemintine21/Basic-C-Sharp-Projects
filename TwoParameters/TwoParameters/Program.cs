354f3230d35ec19cb37704ba78ec7e053c68b779
Stephanie Schulz <ohclemintine21@gmail.com>
2020-10-15 17:44:23 -0700

    Void method

---
+++ TwoParameters/TwoParameters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int int1 = 20;
+            int int2 = 35;
+
+            //instantiate the class
+            VoidMethod myVoidMethod = new VoidMethod();
+            myVoidMethod.TwoIntegers(int1, int2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TwoParameters/TwoParameters/VoidMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoParameters
+{
+    class VoidMethod 
+    {
+        //create a void method that takes two integers as parameters
+        public void TwoIntegers(int int1, int int2)
+        {
+            //call method in class, specifying the parameters by name
+            int int3 = int1 + 70;
+            Console.WriteLine("The first number, " + int1 + " , plus 70 equals " + int3 + ".");
+            Console.WriteLine("The second number is " + int2 + ".");
+        }
+    }
+}
