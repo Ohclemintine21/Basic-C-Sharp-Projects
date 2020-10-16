15ce845d4c633a497aa65306cf004e989c7dc8c6
Stephanie Schulz <ohclemintine21@gmail.com>
2020-10-16 16:45:24 -0700

    abstract assignment

---
+++ AbstractClass/AbstractClass/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    public class Person
+    {
+        public string firstName = "Sample ";
+        public string lastName = "Student";
+        public void SayName()
+        {
+            Console.WriteLine();
+        }
+    }
+}
+        //public List<string> FullName { get; set; }
+        //public string firstName { get; set; }
+        //public string lastName { get; set; }
+        //public string GetFullName()
+        //  {
+        //   return firstName + " " + lastName;
+        //  }
+        //}
+        //public delegate void SayName();
+
---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myPerson = new Person();
+            
+            myPerson.SayName();
+
+            Console.WriteLine("Name: " + myPerson.firstName + myPerson.lastName);
+            Console.ReadLine();
+
+            //Employee person = new Employee();
+            //person.FullName = new List<string>() { "Sample Student" };
+            //person.SayName();
+            //Console.WriteLine("Name: ");
+            //Console.ReadLine();
+
+
+
+        }
+    }
+}
