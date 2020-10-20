d22097744104ae67e94bf3a0f305075484c9c82b
Stephanie Schulz <ohclemintine21@gmail.com>
2020-10-19 19:50:34 -0700

    Overload "==" operator

---
+++ OverloadOperator/OverloadOperator/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperator
+{
+    class Employee : Person
+    {
+        public int id { get; set; }
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            return (emp1.id == emp2.id);
+        }
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return (emp1.id != emp2.id);
+        }
+
+    }
+}
---
+++ OverloadOperator/OverloadOperator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //overload the "==" operator so it checks if two employee objects are equal by comparing id property
+            Employee emp1 = new Employee();
+            emp1.firstName = "Bob";
+            emp1.lastName = "Smith";
+            emp1.id = 1;
+
+            Employee emp2 = new Employee();
+            emp2.firstName = "John";
+            emp2.lastName = "Comesky";
+            emp2.id = 2;
+            Console.WriteLine(emp1 == emp2);
+            Console.WriteLine(emp1 != emp2);
+            Console.ReadLine();
+        }
+    }
+}
