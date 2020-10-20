423686d7a6a5c8151ba589de1ab1a19f249d5415
Stephanie Schulz <ohclemintine21@gmail.com>
2020-10-19 20:47:44 -0700

    Enum Days of the week assignment

---
+++ eNums/eNums/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //create an enum for days of the week
+                DaysOfTheWeek day;
+                Console.WriteLine("Please enter the current day of the week: ");
+                //assign value to a variable of that enum data type created
+                string input = Console.ReadLine(); 
+                day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), input);
+
+                Console.WriteLine("You said: " + input);
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please type an actual day of the week.");
+                Console.ReadLine();
+            }
+        }
+        public enum DaysOfTheWeek
+        {
+            None,
+            Monday = 1,
+            Tuesday = 2,
+            Wednesday = 3,
+            Thursday = 4,
+            Friday = 5,
+            Saturday = 6,
+            Sunday = 7,
+        }
+    }
+}
