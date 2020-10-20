51b04e2b49f7b101120a067c64eed157d03fde11
Stephanie Schulz <ohclemintine21@gmail.com>
2020-10-19 21:05:36 -0700

    Struct decimal assignment

---
+++ Struct/Struct/Number.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    public class Number
+    {
+        public decimal Amount { get; set; }
+        public void DecAmount()
+        {
+            Amount = Decimal.Add(Amount, 4.8m);
+        }
+    }
+}
---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //object data type Number and assign amount
+            Number dec1 = new Number();
+            dec1.Amount = 4.8m;
+            //print amount to console
+            Console.WriteLine(dec1.Amount);
+            Console.ReadLine();
+        }
+    }
+}
