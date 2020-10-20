b40c118b25cea5156046c0d3918f7bce6d6a29bd
Stephanie Schulz <ohclemintine21@gmail.com>
2020-10-19 20:25:15 -0700

    Generic type parameter

---
+++ Generics/Generics/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Employee<T> //generic type parameter
+    {
+        public List<T> Things { get; set; } //add property "Things" be a generic list
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+         static void Main()
+        {
+            //instantiate int as generic parameter
+            Employee<int> emp1 = new Employee<int>();
+            emp1.Things = new List<int>() { 40, 35, 16, 200, 160 } ; //assign int as property value "Things"
+            //instantiate string list as generic parameter
+            Employee<string> emp2 = new Employee<string>();
+            emp2.Things = new List<string>() { "Cookies", "Cupcakes", "Candy" }; //assign string as property value "Things" 
+            //create loop that prints all of Things to console
+            foreach (var thing in emp1.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            foreach (var thing in emp2.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            Console.ReadLine();
+        }
+       
+    }
+}
