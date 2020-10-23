9a5f7a40c15c47bdd909886947d41f834ddbea7b
Stephanie Schulz <ohclemintine21@gmail.com>
2020-10-23 15:10:54 -0700

    Date and time assignment

---
+++ Date and Time/Date and Time/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_and_Time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Asks user for a number 1-9
+            double userInput;
+            Console.WriteLine("Please eneter a numebr from 1-9.");
+            userInput = double.Parse(Console.ReadLine());
+            Console.WriteLine("You said, " + userInput);
+            Console.ReadLine();
+
+            //Prints current date and time to the console
+            Console.WriteLine("The current date and time is: ");
+            var date01 = DateTime.Now;
+            Console.WriteLine(date01);
+            Console.ReadLine();
+
+            //Prints user input from previous step as x hours added to real time
+            Console.WriteLine("The time plus your choice is: ");
+            var resultTime = date01.AddHours(userInput);
+            Console.WriteLine(resultTime);
+            Console.ReadLine();
+        }
+    }
+}
