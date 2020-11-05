9f893c93e307233d733c04d0ed008e1fcfc85dd3
Stephanie Schulz <ohclemintine21@gmail.com>
2020-11-05 13:05:50 -0800

    fixed code errors

---
+++ AbstractClass2/AbstractClass2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass2
+{
+    class Program
+    {
+        static void Main(string[] args, object myObj)
+        {
+            Employee person = new Employee();
+            person.firstName = "Sample";
+            person.lastName = "Student";
+            person.SayName();
+
+            Console.WriteLine("Name: " + person.firstName + "" + person.lastName);
+            Console.ReadLine();
+        }
+    }
+}
