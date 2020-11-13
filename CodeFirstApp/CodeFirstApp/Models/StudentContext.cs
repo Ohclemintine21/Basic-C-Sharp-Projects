8b2ba3bab9c07060cf69a7f1a45a9840122c7dff
Stephanie Schulz <ohclemintine21@gmail.com>
2020-11-12 18:31:59 -0800

    Code first app

---
+++ CodeFirstApp/CodeFirstApp/Models/StudentContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirstApp.Models
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext() : base("StudentDatabase")
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
